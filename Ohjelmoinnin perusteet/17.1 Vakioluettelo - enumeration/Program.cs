abd46c14add0872ea71cef85efacbbd14748e9a6
Inga <inga.varrik@gmail.com>
2021-05-01 23:41:01 +0300

    Tehtava 17.1

--- Ohjelmoinnin perusteet/17.1 Vakioluettelo - enumeration/Program.cs
+++ Ohjelmoinnin perusteet/17.1 Vakioluettelo - enumeration/Program.cs
@@ -7,47 +7,61 @@ namespace _17._1_Vakioluettelo___enumeration
 {
     class Program
     {
-        enum tiedot
+        //Vaihe 1. Tee ohjelma, jossa määrittelet vakioluettelon(enumeration) käyttöliittymän valinnoista siten,
+        //että lisaaNimi = 0, lisaaIka = 1, naytaNimi = 2, jne.  
+        enum Valinta
         {
-            lisaaNimi = 0, lisaaIka = 1, naytaNimi = 2, naytaIkasi = 3, Poistu
+            lisaaNimi = 1, lisaaIka = 2, naytaNimi = 3, naytaIkasi = 4, poistu = 5
         }
         static void Main(string[] args)
         {
-            int tietoa;
 
-            Console.WriteLine("Kirjoitaa arvosanan väliltä (0-3) ");
-             tietoa = int.Parse(Console.ReadLine());
-            string tulos = "";
-            Console.WriteLine("Syötä nimesi: " + tulos);
-            Console.WriteLine("Syötä ikäsi: " + tulos);
-            Console.WriteLine("Näytä nimesi: " + tulos);
-            Console.WriteLine("Näytä ikäsi " + tulos);
-            Console.WriteLine("Poistu ohjelmasta.");
-            switch (tietoa)
+            Valinta valinta;
+
+
+         
+
+            Console.WriteLine("Tervetuloa ohjelmaan. Siirry valikossa valitsemalla numero.");
+            Console.WriteLine("Valinnat ovat:");
+            Console.WriteLine("1 Syötä nimesi");
+            Console.WriteLine("2 Syötä ikäsi");
+            Console.WriteLine("3 Näytä nimesi");
+            Console.WriteLine("4 Näytä ikäsi");
+            Console.WriteLine("5 Poistu ohjelmasta.");
+            Console.WriteLine("");
+            string nimi = "";
+            string ika = "";
+            while (true)
             {
-                case 0:
-                    tulos = " ";
-                    break;
-                case 1:
-                    tulos = " ";
-                    break;
-                case 7:
-                    tulos = " ";
-                    break;
-                case 2:
-                    tulos = " ";
-                    break;
-                case 9:
-                    tulos = " ";
-                    break;
-                case 3:
-                    tulos = "10 on erinomainen arvosana";
-                    break;
-                default:
-                    Console.WriteLine("Annoit virheellisen arvosanan. Arvosanan pitää olla väliltä (0-3) ");
-                    return;
+                //Console.Write("Valitse numero.");
+                Console.Write(">");
+                valinta = (Valinta)Convert.ToInt32(Console.ReadLine());
+                switch (valinta)
+                {
+                    case Valinta.lisaaNimi:
+                        Console.WriteLine("Syötä nimesi: ");
+                        nimi = Console.ReadLine();
+                        Console.WriteLine("Nimesi on tallennettu. ");
+                        break;
+                    case Valinta.lisaaIka:
+                        Console.WriteLine("Syötä ikäsi: ");
+                        ika = Console.ReadLine();
+                        Console.WriteLine("Ikäsi on tallennettu. ");
+                        break;
+                    case Valinta.naytaNimi:
+                        Console.WriteLine("Nimesi on: " + nimi);
+                        break;
+                    case Valinta.naytaIkasi:
+                        Console.WriteLine("Ikäsi on: " + ika);
+                        break;
+                    case Valinta.poistu:
+                        Console.WriteLine("Kiitos ohjelman käytöstä.");
+                        return;
+                    default:
+                        Console.WriteLine("Annoit virheellisen syötteen. Valinnan pitää olla väliltä (1-5) ");
+                        return;
+                }
             }
-            Console.WriteLine(tulos);
         }
     }
 }
