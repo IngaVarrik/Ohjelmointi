606a7a2956e54e1d6ece4f5417d1fd8db15900c8
Inga <inga.varrik@gmail.com>
2021-05-02 16:16:29 +0300

    tehtava 18 lisatty

---
+++ Ohjelmoinnin perusteet/18 Tekstin käsittely/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _18_Tekstin_käsittely
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string merkkijono;
+            Console.WriteLine("Anna näppäimistöltä merkkijono: ");
+            merkkijono = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine(merkkijono);
+
+            char[] array = merkkijono.ToCharArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                // Get character from array. 
+                char letter = array[i];
+                // Display each letter. 
+                Console.Write(letter);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            for (int i = array.Length-1; i >= 0; i--)
+            {
+                // Get character from array. 
+                char letter = array[i];
+                // Display each letter. 
+                Console.Write(letter);
+            }
+            Console.WriteLine();
+        }
+    }
+}
